e67ef8a73af7a116766a496b31ce8c835c008690
andrewfarm <Andrew.Farm@tufts.edu>
2017-06-19 21:51:02 -0400

    shadow bias depends on slope

--- src/shaders/globe_fragment_shader.glsl
+++ src/shaders/globe_fragment_shader.glsl
@@ -1,6 +1,7 @@
 #define AMBIENT_STRENGTH 0.1
 
-#define SHADOW_BIAS 0.001
+#define SHADOW_BIAS_COEF 0.001
+#define SHADOW_BIAS_MAX 0.01
 
 uniform vec3 u_LightDirection; //must be normalized!
 uniform sampler2D u_TextureUnit;
@@ -11,7 +12,8 @@ varying vec3 v_Normal;
 varying vec2 v_TextureCoords;
 
 void main() {
-    float directionalStrength = max(dot(normalize(v_Normal), u_LightDirection), 0.0);
+    vec3 normalizedNormal = normalize(v_Normal);
+    float directionalStrength = max(dot(normalizedNormal, u_LightDirection), 0.0);
 //    vec4 texColor = texture2D(u_TextureUnit, v_TextureCoords);
     vec4 texColor = texture2D(u_ShadowMapUnit, v_PositionInLightSpace.xy);//.r * vec4(v_TextureCoords.xy, 0.0, 1.0);
 
@@ -19,7 +21,10 @@ void main() {
     gl_FragColor = vec4((texColor/* * totalLight*/).rgb, 1.0);//TODO
 //    gl_FragColor = vec4(gl_FragColor.r * v_PositionInLightSpace.xyz, 1.0);
 
-    if (texture2D(u_ShadowMapUnit, v_PositionInLightSpace.xy).z < v_PositionInLightSpace.z + SHADOW_BIAS) {
+    float shadowBias = clamp(
+        abs(SHADOW_BIAS_COEF * tan(acos(dot(normalizedNormal, vec3(0.0, 1.0, 0.0))))),
+        0.0, SHADOW_BIAS_MAX);
+    if (texture2D(u_ShadowMapUnit, v_PositionInLightSpace.xy).z < v_PositionInLightSpace.z + shadowBias) {
         gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
     }
 //    gl_FragColor = vec4(v_PositionInLightSpace.xyz, 1.0);
